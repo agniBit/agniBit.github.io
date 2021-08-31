61892983beb7e79b547ceb4b7b18a5e39f7e7557
Abhishek Agnihotri <agni.abhi00@gmail.com>
2021-08-31 23:58:40 +0530

    add navLinks

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,41 +3,41 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "72c5a0203bc1ba412db74df9358eaf4e",
-"/": "72c5a0203bc1ba412db74df9358eaf4e",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"main.dart.js": "ce8b8db7c7e9190f517d944bb3eb89a9",
-"version.json": "426313f2f3133c2f20415344c4a22df3",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/NOTICES": "3ca94446b3cd9c0985226c5f6f069c60",
-"assets/AssetManifest.json": "00784ec7f87cb1ad31194473e333bf35",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/assets/images/contact_icon.png": "b67aa0ee98a692d7c6312b83c3d12c20",
+  "assets/assets/images/out.gif": "50017548be79dce86ea250906709d191",
 "assets/assets/images/linkdin.png": "0afc9f15984c739994631ce61df05d50",
-"assets/assets/images/TS.png": "a195f0e168b593f8b31b316d12a81978",
-"assets/assets/images/out.gif": "50017548be79dce86ea250906709d191",
-"assets/assets/images/pic_0.png": "d420c41edcfd18134402e4b87f50f764",
-"assets/assets/images/HPE.jpeg": "2725690278bac87c3a891a7d44647564",
-"assets/assets/images/gtkIcon.png": "bd5a8d0c3f09b6384dc2845815c581ba",
-"assets/assets/images/ML.png": "b74210577b91d4cde69741131a8c471c",
-"assets/assets/images/bg2.jpeg": "2879accc54c1b22f91f97d7d650ae43f",
-"assets/assets/images/github.png": "0ba2aa20e2c2ce80e9a2db5b07198464",
-"assets/assets/images/pic_2.png": "a7ce8885e1c83193c21a28e582be4ac8",
-"assets/assets/images/flutterIcon.png": "618037895b03acf73ad93729e2ddb6c7",
+"assets/assets/images/FLD.gif": "2cd02d657975574a0a33daee272fb10b",
 "assets/assets/images/NLP.png": "4109d92629f6831c3fb4f6ec6179fa80",
 "assets/assets/images/CODMenemyDetction.jpeg": "3d683c266b2244112b5f02d82dfa63ed",
-"assets/assets/images/cric.jpg": "9c0904849ca4759b2ec88a8806dc90a5",
-"assets/assets/images/FLD.gif": "2cd02d657975574a0a33daee272fb10b",
-"assets/assets/images/nodeJS.png": "b2b964ab4e4b10d7107daba4ab9ff692",
-"assets/assets/images/reactJS.png": "81b4c60b40fd170b82f27041dd527a1d",
-"assets/assets/images/send_mail.png": "af24bc5136222b43a9d3ca86db787c84",
+"assets/assets/images/gtkIcon.png": "bd5a8d0c3f09b6384dc2845815c581ba",
 "assets/assets/images/pic_1.png": "f3a6694e653499e70868e13e58fa151c",
+"assets/assets/images/send_mail.png": "af24bc5136222b43a9d3ca86db787c84",
+"assets/assets/images/cric.jpg": "9c0904849ca4759b2ec88a8806dc90a5",
+"assets/assets/images/reactJS.png": "81b4c60b40fd170b82f27041dd527a1d",
+"assets/assets/images/HPE.jpeg": "2725690278bac87c3a891a7d44647564",
+"assets/assets/images/pic_2.png": "a7ce8885e1c83193c21a28e582be4ac8",
+"assets/assets/images/contact_icon.png": "b67aa0ee98a692d7c6312b83c3d12c20",
+"assets/assets/images/bg2.jpeg": "2879accc54c1b22f91f97d7d650ae43f",
+"assets/assets/images/flutterIcon.png": "618037895b03acf73ad93729e2ddb6c7",
+"assets/assets/images/ML.png": "b74210577b91d4cde69741131a8c471c",
+"assets/assets/images/TS.png": "a195f0e168b593f8b31b316d12a81978",
+"assets/assets/images/pic_0.png": "d420c41edcfd18134402e4b87f50f764",
+"assets/assets/images/github.png": "0ba2aa20e2c2ce80e9a2db5b07198464",
 "assets/assets/images/bg1.jpeg": "afdcc9520e8b60bebe5c4d255fa6c4ec",
+"assets/assets/images/nodeJS.png": "b2b964ab4e4b10d7107daba4ab9ff692",
 "assets/assets/projectDetails.json": "ab3e4df0c2b00a6622b8d3c7093617bc",
-"manifest.json": "a83bbbd9d23ea4d752fbcd44cdc544fa"
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/AssetManifest.json": "00784ec7f87cb1ad31194473e333bf35",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/NOTICES": "1fe014483ea66f388044e8a519d6df02",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"index.html": "a284c0e1bfd3639235dbccd641e30200",
+"/": "a284c0e1bfd3639235dbccd641e30200",
+"manifest.json": "a83bbbd9d23ea4d752fbcd44cdc544fa",
+"main.dart.js": "bc3d4f3c297df0b989149f8d23e53268",
+"version.json": "426313f2f3133c2f20415344c4a22df3",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -55,7 +55,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
@@ -181,7 +181,7 @@ async function downloadOffline() {
     }
     currentContent[key] = true;
   }
-  for (var resourceKey in Object.keys(RESOURCES)) {
+  for (var resourceKey of Object.keys(RESOURCES)) {
     if (!currentContent[resourceKey]) {
       resources.push(resourceKey);
     }
